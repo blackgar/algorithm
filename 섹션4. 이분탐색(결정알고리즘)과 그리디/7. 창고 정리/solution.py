000387a6d50ebba9f70c650c8194286c4e7fd3c9
윤관 <blackgar@naver.com>
2022-07-22 00:28:34 +0900

    upload 220722 algorithm study || section4 7&8

--- 섹션4. 이분탐색(결정알고리즘)과 그리디/7. 창고 정리/solution.py
+++ 섹션4. 이분탐색(결정알고리즘)과 그리디/7. 창고 정리/solution.py
@@ -5,3 +5,21 @@ sys.stdin = open("input.txt")
 T = int(input())
 
 for tc in range(1, T + 1):
+    l = int(input())
+    arr = list(map(int, input().split()))
+    m = int(input())
+
+    # min, max 사용
+    for i in range(m):
+        arr[arr.index(max(arr))] -= 1
+        arr[arr.index(min(arr))] += 1
+
+    print(max(arr) - min(arr))
+
+    # min, max 사용x
+    for i in range(m):
+        arr.sort()
+        arr[-1] -= 1
+        arr[0] += 1
+    arr.sort()
+    print(arr[-1] - arr[0])
--- 섹션4. 이분탐색(결정알고리즘)과 그리디/8. 침몰하는 타이타닉/solution.py
+++ 섹션4. 이분탐색(결정알고리즘)과 그리디/8. 침몰하는 타이타닉/solution.py
@@ -5,3 +5,25 @@ sys.stdin = open("input.txt")
 T = int(input())
 
 for tc in range(1, T + 1):
+    n, m = map(int, input().split())
+    arr = list(map(int, input().split()))
+    arr.sort(reverse=True)
+    cnt = 0
+
+    while True:
+        # print(arr, cnt)
+        if len(arr) == 0:
+            break
+        elif len(arr) < 2:
+            cnt += 1
+            break
+        tmp = 0
+        for i in range(1, len(arr)):
+            if arr[0] + arr[i] <= m:
+                tmp = arr[i]
+                break
+        if tmp != 0:
+            arr.remove(tmp)
+        arr.remove(arr[0])
+        cnt += 1
+    print(cnt)

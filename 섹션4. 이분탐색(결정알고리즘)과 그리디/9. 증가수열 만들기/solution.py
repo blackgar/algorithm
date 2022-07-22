352efbdf4ca1fc7d4e5161f22ba4ad309dcf4416
윤관 <blackgar@naver.com>
2022-07-22 21:22:18 +0900

    220722 night algorithm study || section4 9

--- 섹션4. 이분탐색(결정알고리즘)과 그리디/9. 증가수열 만들기/solution.py
+++ 섹션4. 이분탐색(결정알고리즘)과 그리디/9. 증가수열 만들기/solution.py
@@ -5,3 +5,30 @@ sys.stdin = open("input.txt")
 T = int(input())
 
 for tc in range(1, T + 1):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    tmp = 0
+    answer = ''
+
+    while True:
+        if (arr[0] < tmp and arr[-1] < tmp) or len(arr) == 0:
+            break
+        if arr[0] > tmp and arr[-1] < tmp:
+            tmp = arr[0]
+            arr = arr[1:]
+            answer += "L"
+        elif arr[-1] > tmp and arr[0] < tmp:
+            tmp = arr[-1]
+            arr = arr[:-1]
+            answer += "R"
+        elif arr[0] > tmp and arr[0] < arr[-1]:
+            tmp = arr[0]
+            arr = arr[1:]
+            answer += "L"
+        elif arr[-1] > tmp and arr[0] > arr[-1]:
+            tmp = arr[-1]
+            arr = arr[:-1]
+            answer += "R"
+
+    print(len(answer))
+    print(answer)

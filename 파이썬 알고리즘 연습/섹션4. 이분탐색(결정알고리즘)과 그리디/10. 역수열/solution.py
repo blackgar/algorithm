1e2cd6b317f32cf10fe3d46d7d7b77b543162447
윤관 <blackgar@naver.com>
2022-07-25 16:12:01 +0900

    220725 upload algorithm study || section4 10

--- 파이썬 알고리즘 연습/섹션4. 이분탐색(결정알고리즘)과 그리디/10. 역수열/solution.py
+++ 파이썬 알고리즘 연습/섹션4. 이분탐색(결정알고리즘)과 그리디/10. 역수열/solution.py
@@ -10,6 +10,22 @@ for tc in range(1, T + 1):
     # 4=>0
     # 5=>3이어야하는데 이미 2가 그 자리에 있으므로 4
     # 6=>2 7=>7 8=>1
-
-
-
+    n = int(input())
+    arr = list(map(int, input().split()))
+    origin = [0] * n
+    # print(n, arr, origin)
+    for i in range(n):
+        cnt = 0
+        tmp = 0
+        for j in range(n):
+            if cnt >= arr[i]:
+                if origin[cnt+tmp]:
+                    tmp += 1
+                    continue
+                origin[cnt+tmp] = i+1
+                break
+            if origin[j] == 0:
+                cnt += 1
+            else:
+                tmp += 1
+    print(origin)

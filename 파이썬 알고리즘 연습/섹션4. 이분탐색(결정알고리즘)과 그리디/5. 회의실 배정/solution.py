c618ad22e1ec0b234067da7a386232a6dc1c1ffe
윤관 <blackgar@naver.com>
2022-07-26 13:11:39 +0900

    upload 220726 algorithm study || section4 5

--- 파이썬 알고리즘 연습/섹션4. 이분탐색(결정알고리즘)과 그리디/5. 회의실 배정/solution.py
+++ 파이썬 알고리즘 연습/섹션4. 이분탐색(결정알고리즘)과 그리디/5. 회의실 배정/solution.py
@@ -8,13 +8,20 @@ for tc in range(1, T + 1):
     n = int(input())
     arr = [list(map(int, input().split())) for _ in range(n)]
     arr.sort()
-    tmp = arr[0][0]
-    answer = [arr[0]]
+    # 시작시간이 동일하지만 가장 일찍 끝나는 거 찾기
+    # 그 일찍 끝나는거 이어서 바로 시작하거나 제일 가깝게 시작하는거 찾기
+    # 단 찾을때 시작시간이 가까워도 끝나는 시간이 길어서 시작시간과 멀어도 끝나는 시간이 가까운 것을 선택하기
+    # 그렇게 해서 연속으로 진행되는 회의 찾기
+    st = arr[0][0]
+    et = arr[0][1]
+    cnt = 1
+    tmp = 0
     for i in range(n):
-        print(arr[i])
-        if arr[i][0] == tmp:
-            continue
-        elif arr[i][0] == tmp and arr[i][1] > answer[-1][1]:
-            tmp = arr[i][0]
-            answer.append(arr[i])
-    print(len(answer))
+        if arr[i][0] >= st and arr[i][1] < et:
+            st = arr[i][0]
+            et = arr[i][1]
+        elif arr[i][0] >= et:
+            cnt += 1
+            st = arr[i][0]
+            et = arr[i][1]
+    print(cnt)

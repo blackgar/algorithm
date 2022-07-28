62b59769dcd5475c08b18758d1151b53e8b815b8
윤관 <blackgar@naver.com>
2022-07-28 23:29:46 +0900

    upload 220728 algorithm study || section5 1~2

--- 파이썬 알고리즘 연습/섹션4. 이분탐색(결정알고리즘)과 그리디/4. 마구간 정하기/solution.py
+++ 파이썬 알고리즘 연습/섹션4. 이분탐색(결정알고리즘)과 그리디/4. 마구간 정하기/solution.py
@@ -13,23 +13,29 @@ for tc in range(1, T + 1):
     # 안되면 거리 반으로 줄인 후 개수가 들어가는지 확인한다.
     # 된다면 거기서부터 +1 시키면서 들어가는지 확인한다.
     # 안되는 순간이 오면 그 때의 거리 -1이 최대 거리이다.
-    min_num = min(arr)
-    max_num = max(arr)
-    mid = (min_num + max_num) // (c-1)
+    distance = (max(arr)-min(arr)) // c
     cnt = 1
-    answer = 0
+    answer = n
     while True:
+        result = [arr[0]]
         min_num = min(arr)
         max_num = max(arr)
         for i in range(1, n):
-            if arr[i]-min_num > mid:
+            if arr[i]-min_num > distance:
                 cnt += 1
+                result.append(arr[i])
                 min_num = arr[i]
-                if cnt == c:
+                if cnt > c:
                     break
         if cnt == c:
-            mid += 1
-        if cnt < c:
-            answer = mid - 1
+            distance += 1
+        elif cnt < c:
+            distance // 2
+        elif cnt > c:
             break
+
+    for i in range(len(result)-1):
+        if arr[i+1] - arr[i] < answer:
+            answer = arr[i+1] - arr[i]
+
     print(answer)
--- 파이썬 알고리즘 연습/섹션5. 자료구조활용(스택, 큐, 해쉬, 힙)/1. 가장 큰 수/solution.py
+++ 파이썬 알고리즘 연습/섹션5. 자료구조활용(스택, 큐, 해쉬, 힙)/1. 가장 큰 수/solution.py
@@ -9,6 +9,7 @@ for tc in range(1, T + 1):
     number = list(map(int, str(n)))
     stack = []
 
+    # 스택에 넣어 비교하면서 어떤 숫자들이 남아야 하는지 비교하면서 남긴 수들을 문자열로 만들어준다.
     for i in number:
         while stack and m > 0 and stack[-1] < i:
             stack.pop()

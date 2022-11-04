bb956af9e12046110b828f2e978297914fae5777
Yoon Gwan <blackgar@naver.com>
2022-11-05 06:00:57 +0900

    섹션5. 단어찾기. 배열 & 딕셔너리

--- 파이썬 알고리즘 연습/섹션5. 자료구조활용(스택, 큐, 해쉬, 힙)/8. 단어찾기/solution2.py
+++ 파이썬 알고리즘 연습/섹션5. 자료구조활용(스택, 큐, 해쉬, 힙)/8. 단어찾기/solution2.py
@@ -5,3 +5,32 @@ sys.stdin = open("input.txt")
 T = int(input())
 
 for tc in range(1, T + 1):
+    n = int(input())
+    word_list = [input() for _ in range(n)]
+    poem_word = [input() for _ in range(n-1)]
+
+    # 정렬 활용 방법
+    word_list.sort()
+    poem_word.sort()
+    for i in range(n):
+        if i == n-1:
+            print(word_list[i])
+        if word_list[i] != poem_word[i]:
+                print(word_list[i])
+                break
+
+
+    # 정렬 x, 딕셔너리 활용
+    dictionary = dict()
+    for i in word_list:
+        dictionary[i] = 1
+    for i in poem_word:
+        dictionary[i] = 0
+    for key, val in dictionary.items():
+        if val == 1:
+            print(key)
+            break
+
+
+
+

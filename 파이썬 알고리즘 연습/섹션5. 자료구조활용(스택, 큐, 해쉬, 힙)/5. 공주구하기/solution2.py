4cc412ae3bf4de71d5008e38551980fce52700ce
Yoon Gwan <blackgar@naver.com>
2022-11-05 04:27:33 +0900

    세션 5. 공주구하기

--- 파이썬 알고리즘 연습/섹션5. 자료구조활용(스택, 큐, 해쉬, 힙)/5. 공주구하기/solution2.py
+++ 파이썬 알고리즘 연습/섹션5. 자료구조활용(스택, 큐, 해쉬, 힙)/5. 공주구하기/solution2.py
@@ -1,7 +1,24 @@
 import sys
-
+from collections import deque
 sys.stdin = open("input.txt")
 
 T = int(input())
 
 for tc in range(1, T + 1):
+    n, k = map(int, input().split())
+    prince = [i for i in range(1, n+1)]
+    # deque 활용하는 방법
+    dq = deque(prince)
+    cnt = 0
+    while len(prince) > 1:
+        cnt += 1
+        if cnt % k == 0:
+            # deque로 popleft 쓰거나
+            # 그냥 배열에서 pop(index)로 처리할 수 있다.
+            prince.pop(0)
+            dq.popleft()
+        else:
+            dq.append(dq.popleft())
+            prince.append(prince.pop(0))
+    print(prince[0])
+    print(dq[0])

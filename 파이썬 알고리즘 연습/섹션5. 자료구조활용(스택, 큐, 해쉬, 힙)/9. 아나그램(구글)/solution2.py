edbc29872bc0308cb9d59c15bd38bec52c06e17d
Yoon Gwan <blackgar@naver.com>
2022-11-05 06:12:41 +0900

    섹션 5. 아나그램

--- 파이썬 알고리즘 연습/섹션5. 자료구조활용(스택, 큐, 해쉬, 힙)/9. 아나그램(구글)/solution2.py
+++ 파이썬 알고리즘 연습/섹션5. 자료구조활용(스택, 큐, 해쉬, 힙)/9. 아나그램(구글)/solution2.py
@@ -5,3 +5,24 @@ sys.stdin = open("input.txt")
 T = int(input())
 
 for tc in range(1, T + 1):
+    ana = dict()
+    answer = "YES"
+    for i in input():
+        # dictionary에서 key가 없을 때는 생성해주고 있을 때는 그 키에 해당하는 값에 접근할 수 있게 해주는 함수 get
+        ana[i] = ana.get(i, 0) + 1
+        # 위 함수와 아래 조건문이 같은 동작을 한다.
+        if i in ana.keys():
+            ana[i] += 1
+        else:
+            ana[i] = 1
+    print(ana)
+    for j in input():
+        ana[j] -= 1
+        # 위 코드와 동일한 방법
+        ana[j] = ana.get(j, 0) - 1
+    print(ana)
+    for key, val in ana.items():
+        if val != 0:
+            answer = "NO"
+            break
+    print(answer)

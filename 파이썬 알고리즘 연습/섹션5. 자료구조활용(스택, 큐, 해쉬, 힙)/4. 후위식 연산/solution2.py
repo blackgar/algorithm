85c98ff2bb0d756cf1dcd41124a9d78de6a7c740
Yoon Gwan <blackgar@naver.com>
2022-11-05 02:33:28 +0900

    세션 5. 후위식 연산 stack으로 풀기, stack 아닌 인덱스 접근으로 풀기

--- 파이썬 알고리즘 연습/섹션5. 자료구조활용(스택, 큐, 해쉬, 힙)/4. 후위식 연산/solution2.py
+++ 파이썬 알고리즘 연습/섹션5. 자료구조활용(스택, 큐, 해쉬, 힙)/4. 후위식 연산/solution2.py
@@ -5,28 +5,47 @@ sys.stdin = open("input.txt")
 T = int(input())
 
 for tc in range(1, T + 1):
-    calculate = list(input())
+    # calculate = list(input())
+    calculate = input()
     i = 0
     stack = []
     operator = ["+", "-", "*", "/"]
-    while True:
-        tmp = 0
-        if calculate[i] == "+":
-            tmp = int(calculate[i-2]) + int(calculate[i-1])
-        elif calculate[i] == "-":
-            tmp = int(calculate[i - 2]) - int(calculate[i - 1])
-        elif calculate[i] == "*":
-            tmp = int(calculate[i - 2]) * int(calculate[i - 1])
-        elif calculate[i] == "/":
-            tmp = int(calculate[i - 2]) / int(calculate[i - 1])
+    # stack이 아닌 풀이 방법
+    # while True:
+    #     tmp = 0
+    #     if calculate[i] == "+":
+    #         tmp = int(calculate[i-2]) + int(calculate[i-1])
+    #     elif calculate[i] == "-":
+    #         tmp = int(calculate[i - 2]) - int(calculate[i - 1])
+    #     elif calculate[i] == "*":
+    #         tmp = int(calculate[i - 2]) * int(calculate[i - 1])
+    #     elif calculate[i] == "/":
+    #         tmp = int(calculate[i - 2]) / int(calculate[i - 1])
+    #
+    #     if calculate[i] in operator:
+    #         calculate = calculate[:i - 2] + [tmp] + calculate[i + 1:]
+    #         i -= 2
+    #     if i == len(calculate)-1:
+    #         break
+    #
+    #     i += 1
+    #
+    # print(calculate[-1])
 
-        if calculate[i] in operator:
-            calculate = calculate[:i - 2] + [tmp] + calculate[i + 1:]
-            i -= 2
+    # stack으로 푼 방법
+    for x in calculate:
+        if x.isdecimal():
+            stack.append(x)
+        else:
+            tmp1 = int(stack.pop())
+            tmp2 = int(stack.pop())
+            if x == "+":
+                stack.append(tmp2 + tmp1)
+            elif x == "-":
+                stack.append(tmp2 - tmp1)
+            elif x == "*":
+                stack.append(tmp2 * tmp1)
+            elif x == "/":
+                stack.append(tmp2 / tmp1)
 
-        if i == len(calculate)-1:
-            break
-
-        i += 1
-
-    print(calculate[-1])
+    print(stack)

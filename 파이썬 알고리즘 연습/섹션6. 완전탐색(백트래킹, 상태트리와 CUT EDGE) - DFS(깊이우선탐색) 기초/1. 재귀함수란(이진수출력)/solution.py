1eda17a9b342bc59fe53145b8d9170d39d9d4fc0
Yoon Gwan <blackgar@naver.com>
2022-11-05 13:50:23 +0900

    섹션 6. 재귀함수란

--- 파이썬 알고리즘 연습/섹션6. 완전탐색(백트래킹, 상태트리와 CUT EDGE) - DFS(깊이우선탐색) 기초/1. 재귀함수란(이진수출력)/solution.py
+++ 파이썬 알고리즘 연습/섹션6. 완전탐색(백트래킹, 상태트리와 CUT EDGE) - DFS(깊이우선탐색) 기초/1. 재귀함수란(이진수출력)/solution.py
@@ -2,6 +2,23 @@ import sys
 
 sys.stdin = open("input.txt")
 
-T = int(input())
+def make_bin(x):
+    global answer
+    if x == 0:
+        return
+    else:
+        make_bin(x // 2)
+        answer += str(x % 2)
+    # 23 => make_bin(11) => make_bin(5) => make_bin(2) => make_bin(1) => make_bin(0) return
+    # make_bin(1), answer += 1 => make_bin(2), answer += 0 => make_bin(5), answer += 1 =>
+    # make_bin(11), answer += 1 => make_bin(23), answer += 1
+    # => answer = 10111
 
-for tc in range(1, T + 1):
+if __name__=="__main__":
+    T = int(input())
+    for tc in range(1, T + 1):
+        print(tc, "번째 test")
+        n = int(input())
+        answer = ""
+        make_bin(n)
+        print(answer)

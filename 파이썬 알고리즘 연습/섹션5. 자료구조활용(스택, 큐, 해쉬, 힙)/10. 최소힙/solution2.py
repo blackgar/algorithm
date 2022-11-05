4ef4800c83de77d734686a79a84fc229f5f8d669
Yoon Gwan <blackgar@naver.com>
2022-11-05 13:25:27 +0900

    섹션 5. 최소힙, 최대힙

--- 파이썬 알고리즘 연습/섹션5. 자료구조활용(스택, 큐, 해쉬, 힙)/10. 최소힙/solution2.py
+++ 파이썬 알고리즘 연습/섹션5. 자료구조활용(스택, 큐, 해쉬, 힙)/10. 최소힙/solution2.py
@@ -1,7 +1,22 @@
 import sys
+import heapq as hq
 
 sys.stdin = open("input.txt")
 
 T = int(input())
 
 for tc in range(1, T + 1):
+    print(tc, "번째 test")
+    a = []
+    while True:
+        n = int(input())
+        if n == -1:
+            break
+        if n == 0:
+            if len(a) == 0:
+                print(-1)
+            else:
+                print(hq.heappop(a))
+        else:
+            hq.heappush(a, n)
+    # print(a)

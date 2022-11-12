1289452423f4f228449d122fe1855adea45034d9
Yoon Gwan <blackgar@naver.com>
2022-11-12 23:24:29 +0900

    섹션 6. 이진트리순회

--- 파이썬 알고리즘 연습/섹션6. 완전탐색(백트래킹, 상태트리와 CUT EDGE) - DFS(깊이우선탐색) 기초/2. 이진트리순회/solution2.py
+++ 파이썬 알고리즘 연습/섹션6. 완전탐색(백트래킹, 상태트리와 CUT EDGE) - DFS(깊이우선탐색) 기초/2. 이진트리순회/solution2.py
@@ -2,6 +2,25 @@ import sys
 
 sys.stdin = open("input.txt")
 
-T = int(input())
+def dfs(x, i):
+    if x == 3:
+        return
+    else:
+        # 전위 순회
+        print(i, end=" ")
+        dfs(x+1, i*2)
+        # 중위 순회
+        print(i, end=" ")
+        dfs(x+1, i*2+1)
+        # 후위 순회
+        print(i, end=" ")
 
-for tc in range(1, T + 1):
+def dfs2(x):
+    if x > 7:
+        return
+    else:
+        print(x, end=" ")
+        dfs2(x*2)
+        dfs2(x*2+1)
+# dfs(0, 1)
+dfs2(1)

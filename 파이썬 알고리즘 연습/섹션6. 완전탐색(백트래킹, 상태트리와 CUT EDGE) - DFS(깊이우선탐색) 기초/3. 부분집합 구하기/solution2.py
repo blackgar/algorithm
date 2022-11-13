b6a2b058297c1854bbaf4cdae17fcbcc0662bb09
Yoon Gwan <blackgar@naver.com>
2022-11-13 23:49:24 +0900

    섹션 6. 부분집합 구하기 solution2

--- 파이썬 알고리즘 연습/섹션6. 완전탐색(백트래킹, 상태트리와 CUT EDGE) - DFS(깊이우선탐색) 기초/3. 부분집합 구하기/solution2.py
+++ 파이썬 알고리즘 연습/섹션6. 완전탐색(백트래킹, 상태트리와 CUT EDGE) - DFS(깊이우선탐색) 기초/3. 부분집합 구하기/solution2.py
@@ -2,6 +2,25 @@ import sys
 
 sys.stdin = open("input.txt")
 
+# 1 2 3
+def dfs(x):
+    if x > n:
+        for i in range(1, n+1):
+            if visited[i]:
+                print(i, end=" ")
+        print()
+        return
+    else:
+        # 해당 숫자 x를 선택하고 다음 숫자를 선택하러 간다.
+        visited[x] = 1
+        dfs(x+1)
+        # 해당 숫자를 선택하지 않고 다음 숫자를 선택하러 간다.
+        visited[x] = 0
+        dfs(x+1)
+
 T = int(input())
 
 for tc in range(1, T + 1):
+    n = int(input())
+    visited = [0] * (n+1)
+    dfs(1)
